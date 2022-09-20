b25316a37d4fe600ec51bb20230310011f97be63
APANG <apangkko@gmail.com>
2022-09-20 19:58:26 +0700

    v3

--- script.js
+++ script.js
@@ -6,25 +6,20 @@ const faqAns = document.querySelectorAll(".faq-a");
 
 for (let i = 0; i < faqAcc.length; i++) {
 
-    faqAcc[i].addEventListener("click", () => {
+    faqAcc[i].addEventListener("mouseover", () => boxIcon.style.left = "-14%");
+    faqAcc[i].addEventListener("mouseout", () => boxIcon.style.left = "-10%");
 
+    faqAcc[i].addEventListener("click", () => {
 
         if (faqQ[i].classList.contains('active')) { 
             closeAcc();
-            boxIcon.classList.remove("active");
-            //boxIcon.style.left = "-10%"; 
         }
         else { 
-            //boxIcon.style.left = "-14%"; 
             closeAcc(); 
-            boxIcon.classList.toggle("active");
             faqQ[i].classList.toggle("active");
             faqArrow[i].classList.toggle("active");
             faqAns[i].classList.toggle("active");
-        }
-        
-       
-        
+        }  
         
     });
 }
