a91802a6d19eaed2f986b62cd06841f6fc5e6549
APANG <apangkko@gmail.com>
2022-09-20 18:30:30 +0700

    v2

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ const faqArrow = document.querySelectorAll(".arrow");
 const faqAns = document.querySelectorAll(".faq-a");
 
 for (let i = 0; i < faqAcc.length; i++) {
-    faqAcc[i].addEventListener("mouseover", () => { boxIcon.style.left = "-14%" });
-    faqAcc[i].addEventListener("mouseout", () => { boxIcon.style.left = "-10%" });
+
     faqAcc[i].addEventListener("click", () => {
-        faqQ[i].classList.toggle("active");
-        faqArrow[i].classList.toggle("active");
-        faqAns[i].classList.toggle("active");
+
+
+        if (faqQ[i].classList.contains('active')) { 
+            closeAcc();
+            boxIcon.classList.remove("active");
+            //boxIcon.style.left = "-10%"; 
+        }
+        else { 
+            //boxIcon.style.left = "-14%"; 
+            closeAcc(); 
+            boxIcon.classList.toggle("active");
+            faqQ[i].classList.toggle("active");
+            faqArrow[i].classList.toggle("active");
+            faqAns[i].classList.toggle("active");
+        }
+        
+       
+        
+        
+    });
+}
+
+function closeAcc() {
+    faqQ.forEach(item => {
+        item.classList.remove("active");
+    });
+
+    faqArrow.forEach(item => {
+        item.classList.remove("active");
+    });
+
+    faqAns.forEach(item => {
+        item.classList.remove("active");
     });
 }
